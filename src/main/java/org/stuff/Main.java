f5f7b8271022be758e5e558e6e334393f571c8b1
myrka1 <myrkag93@gmail.com>
2024-01-11 21:15:50 -0800

    lists setup, parameter method and testing

--- src/main/java/org/stuff/Main.java
+++ src/main/java/org/stuff/Main.java
@@ -1,7 +1,73 @@
 package org.stuff;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //System.out.println("Hello world!");
+        //creation of parameters
+        //list of tokens
+        List<String> validAuthTokens = new ArrayList<>() {
+            {
+                add("ah37j2ha483u");
+                add("safh34ywb0p5");
+                add("ba34wyi8t902");
+            }
+        };
+        /* validAuthTokens.add("ah37j2ha483u");
+        validAuthTokens.add("safh34ywb0p5");
+        validAuthTokens.add("ba34wyi8t902"); */
+        List<String> request1 = new ArrayList<>() {{
+            add("GET");
+            add("https://example.com/?token=347sd6yk8iu2&name=alex");
+        }};
+        List<String> request2 = new ArrayList<>() {{
+            add("GET");
+            add("https://example.com/?token=safh34ywb0p5&name=sam");
+        }};
+        List<String> request3 = new ArrayList<>() {{
+            add("POST");
+            add("https://example.com/?token=safh34ywb0p5&name=alex");
+        }};
+        List<String> request4 = new ArrayList<>() {{
+            add("POST");
+            add("https://example.com/?token=safh34ywb0p5&csrf=ak2sh32dy&name=riley&name=alex");
+        }};
+        //list of request lists
+        List<List<String>> requests = new ArrayList<>() {{
+            add(request1);
+            add(request2);
+            add(request3);
+            add(request4);
+        }};
+    }
+
+    //method to check url and returns list
+    public static List<String> getResponses(List<String> valid_auth_tokens,
+                                            List<List<String>> requests) {
+        List<String> responses = new ArrayList<>();
+        return responses;
+    }
+
+    //url breakdown returns map
+    public static Map<String,String> parameters(String url) {
+        Map<String,String> queryParameters = new HashMap<>();
+
+        int startX = url.indexOf('?');
+        if(startX != -1) {
+            String query = url.substring(startX + 1); //separates the parameter section(Tokens too) from rest of url
+
+            String[] diffParameters = query.split("&"); //query part is split into different parameters
+            for(String str : diffParameters) {
+                String[] sections = str.split("="); //splits each parameter into value and key, ex token and actual token
+                if(sections.length == 2) {
+                    queryParameters.put(sections[0],sections[1]);
+                }
+            }
+        }
+        return queryParameters;
     }
 }
---
+++ src/test/java/org/stuff/MainTest.java
@@ -0,0 +1,63 @@
+package org.stuff;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import static org.junit.Assert.*;
+
+public class MainTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void main() {
+        List<String> validAuthTokens = new ArrayList<>() {
+            {
+                add("ah37j2ha483u");
+                add("safh34ywb0p5");
+                add("ba34wyi8t902");
+            }
+        };
+        List<String> request4 = new ArrayList<>() {{
+            add("POST");
+            add("https://example.com/?token=safh34ywb0p5&csrf=ak2sh32dy&name=chris&name=alex");
+        }};
+        List<List<String>> requests = new ArrayList<>();
+        requests.add(request4);
+    }
+
+    @Test
+    public void getResponses() {
+    }
+
+    @Test
+    public void parameters() {
+        String url = "https://example.com/?token=safh34ywb0p5&csrf=ak2sh32dy&name=riley&name=alex";
+        Map<String,String> testMap = Main.parameters(url);
+        for (Map.Entry<String, String> entry : testMap.entrySet()) {
+            String key = entry.getKey();
+            String value = entry.getValue();
+            if(key.equals("token")) assertEquals("safh34ywb0p5", value);
+            if(key.equals("csrf")) assertEquals("ak2sh32dy", value);
+            if(key.equals("name")) assertTrue(value.equals("riley") || value.equals("alex"));
+
+        }
+        /*String url1 = "https://example.com/?token=abc&name=John";
+        Map<String, String> params1 = YourClassName.parameters(url1);
+        assertEquals(2, params1.size());
+        assertEquals("abc", params1.get("token"));
+        assertEquals("John", params1.get("name"));
+
+        // Test case 2: URL without parameters
+        String url2 = "https://example.com/";
+        Map<String, String> params2 = YourClassName.parameters(url2);
+        assertEquals(0, params2.size());*/
+
+    }
+}
